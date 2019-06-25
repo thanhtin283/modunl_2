43d83d8208710bd18c362d21c92599c5340c9fd3
thanhtin283 <thanhtin283@gmail.com>
2019-06-25 08:26:54 +0700

    a

---
+++ ServerTime/src/ServerTimeServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "ServerTimeServlet", urlPatterns = "/index")
+public class ServerTimeServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    }
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        Date today = new Date();
+
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("</html>");
+
+        writer.println("<html>");
+        writer.println("<h2>HELLO THANH TÍN</h2>");
+        writer.println("<html>");
+
+    }
+}
